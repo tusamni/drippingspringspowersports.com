0a563b03feef570196cbf13603ad602da834af70
Erik Olsen <erik@eriksolsen.com>
2025-11-28 16:27:01 -0600

    commit

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+    adapter: netlify(),
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Poppins",
+            weights: ["300 900"],
+            cssVariable: "--font-heading"
+        }, {
+            provider: fontProviders.google(),
+            name: "Manrope",
+            weights: ["200 800"],
+            cssVariable: "--font-manrope"
+        }]
+    },
+    integrations: [sitemap()],
+    site: "https://eriksolsen.com",
+    trailingSlash: "always",
+    vite: {
+        plugins: [tailwindcss()]
+    },
+});
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+	name: "Dripping Springs Outdoor Powersports",
+	nameShort: "DS Outdoor Powersports",
+	phone: {
+		base: "+15126090471",
+		formatted: "(512) 609-0471",
+	},
+	email: {
+		base: "chris@drippingspringspowersports.com",
+	},
+	url: "https://drippingspringspowersports.com",
+	description: "Your premier destination for outdoor powersports in Dripping Springs, TX.",
+};
